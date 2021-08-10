9f9ddb32d40c1c33520d76531aa49e3b876e4a7c
Phuc Do <phucdt@dlsinc.com>
2021-08-10 10:46:26 +0700

    v

--- neural_style/neural_style.py
+++ neural_style/neural_style.py
@@ -17,103 +17,102 @@ from vgg16 import Vgg16
 
 
 def train(args):
-    with torch.no_grad():
-        np.random.seed(args.seed)
-        torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
 
-        if args.cuda:
-            torch.cuda.manual_seed(args.seed)
-            kwargs = {'num_workers': 0, 'pin_memory': False}
-        else:
-            kwargs = {}
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+        kwargs = {'num_workers': 0, 'pin_memory': False}
+    else:
+        kwargs = {}
 
-        transform = transforms.Compose([transforms.Scale(args.image_size),
-                                        transforms.CenterCrop(args.image_size),
-                                        transforms.ToTensor(),
-                                        transforms.Lambda(lambda x: x.mul(255))])
-        train_dataset = datasets.ImageFolder(args.dataset, transform)
-        train_loader = DataLoader(train_dataset, batch_size=args.batch_size, **kwargs)
+    transform = transforms.Compose([transforms.Scale(args.image_size),
+                                    transforms.CenterCrop(args.image_size),
+                                    transforms.ToTensor(),
+                                    transforms.Lambda(lambda x: x.mul(255))])
+    train_dataset = datasets.ImageFolder(args.dataset, transform)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, **kwargs)
 
-        transformer = TransformerNet()
-        optimizer = Adam(transformer.parameters(), args.lr)
-        mse_loss = torch.nn.MSELoss()
+    transformer = TransformerNet()
+    optimizer = Adam(transformer.parameters(), args.lr)
+    mse_loss = torch.nn.MSELoss()
 
-        vgg = Vgg16()
-        utils.init_vgg16(args.vgg_model_dir)
-        vgg.load_state_dict(torch.load(os.path.join(args.vgg_model_dir, "vgg16.weight")))
+    vgg = Vgg16()
+    utils.init_vgg16(args.vgg_model_dir)
+    vgg.load_state_dict(torch.load(os.path.join(args.vgg_model_dir, "vgg16.weight")))
 
-        if args.cuda:
-            transformer.cuda()
-            vgg.cuda()
+    if args.cuda:
+        transformer.cuda()
+        vgg.cuda()
 
-        style = utils.tensor_load_rgbimage(args.style_image, size=args.style_size)
-        style = style.repeat(args.batch_size, 1, 1, 1)
-        style = utils.preprocess_batch(style)
-        if args.cuda:
-            style = style.cuda()
-        style_v = Variable(style, volatile=True)
-        style_v = utils.subtract_imagenet_mean_batch(style_v)
-        features_style = vgg(style_v)
-        gram_style = [utils.gram_matrix(y) for y in features_style]
+    style = utils.tensor_load_rgbimage(args.style_image, size=args.style_size)
+    style = style.repeat(args.batch_size, 1, 1, 1)
+    style = utils.preprocess_batch(style)
+    if args.cuda:
+        style = style.cuda()
+    style_v = Variable(style, volatile=True)
+    style_v = utils.subtract_imagenet_mean_batch(style_v)
+    features_style = vgg(style_v)
+    gram_style = [utils.gram_matrix(y) for y in features_style]
 
-        for e in range(args.epochs):
-            transformer.train()
-            agg_content_loss = 0.
-            agg_style_loss = 0.
-            count = 0
-            for batch_id, (x, _) in enumerate(train_loader):
-                n_batch = len(x)
-                count += n_batch
-                optimizer.zero_grad()
-                x = Variable(utils.preprocess_batch(x))
-                if args.cuda:
-                    x = x.cuda()
+    for e in range(args.epochs):
+        transformer.train()
+        agg_content_loss = 0.
+        agg_style_loss = 0.
+        count = 0
+        for batch_id, (x, _) in enumerate(train_loader):
+            n_batch = len(x)
+            count += n_batch
+            optimizer.zero_grad()
+            x = Variable(utils.preprocess_batch(x))
+            if args.cuda:
+                x = x.cuda()
 
-                y = transformer(x)
+            y = transformer(x)
 
-                xc = Variable(x.data.clone(), volatile=True)
+            xc = Variable(x.data.clone(), volatile=True)
 
-                y = utils.subtract_imagenet_mean_batch(y)
-                xc = utils.subtract_imagenet_mean_batch(xc)
+            y = utils.subtract_imagenet_mean_batch(y)
+            xc = utils.subtract_imagenet_mean_batch(xc)
 
-                features_y = vgg(y)
-                features_xc = vgg(xc)
+            features_y = vgg(y)
+            features_xc = vgg(xc)
 
-                f_xc_c = Variable(features_xc[1].data, requires_grad=False)
+            f_xc_c = Variable(features_xc[1].data, requires_grad=False)
 
-                content_loss = args.content_weight * mse_loss(features_y[1], f_xc_c)
+            content_loss = args.content_weight * mse_loss(features_y[1], f_xc_c)
 
-                style_loss = 0.
-                for m in range(len(features_y)):
-                    gram_s = Variable(gram_style[m].data, requires_grad=False)
-                    gram_y = utils.gram_matrix(features_y[m])
-                    style_loss += args.style_weight * mse_loss(gram_y, gram_s[:n_batch, :, :])
+            style_loss = 0.
+            for m in range(len(features_y)):
+                gram_s = Variable(gram_style[m].data, requires_grad=False)
+                gram_y = utils.gram_matrix(features_y[m])
+                style_loss += args.style_weight * mse_loss(gram_y, gram_s[:n_batch, :, :])
 
-                total_loss = content_loss + style_loss
-                total_loss.backward()
-                optimizer.step()
+            total_loss = content_loss + style_loss
+            total_loss.backward()
+            optimizer.step()
 
-                agg_content_loss += content_loss.data[0]
-                agg_style_loss += style_loss.data[0]
+            agg_content_loss += content_loss.data[0]
+            agg_style_loss += style_loss.data[0]
 
-                if (batch_id + 1) % args.log_interval == 0:
-                    mesg = "{}\tEpoch {}:\t[{}/{}]\tcontent: {:.6f}\tstyle: {:.6f}\ttotal: {:.6f}".format(
-                        time.ctime(), e + 1, count, len(train_dataset),
-                                    agg_content_loss / (batch_id + 1),
-                                    agg_style_loss / (batch_id + 1),
-                                    (agg_content_loss + agg_style_loss) / (batch_id + 1)
-                    )
-                    print(mesg)
+            if (batch_id + 1) % args.log_interval == 0:
+                mesg = "{}\tEpoch {}:\t[{}/{}]\tcontent: {:.6f}\tstyle: {:.6f}\ttotal: {:.6f}".format(
+                    time.ctime(), e + 1, count, len(train_dataset),
+                                  agg_content_loss / (batch_id + 1),
+                                  agg_style_loss / (batch_id + 1),
+                                  (agg_content_loss + agg_style_loss) / (batch_id + 1)
+                )
+                print(mesg)
 
-        # save model
-        transformer.eval()
-        transformer.cpu()
-        save_model_filename = "epoch_" + str(args.epochs) + "_" + str(time.ctime()).replace(' ', '_') + "_" + str(
-            args.content_weight) + "_" + str(args.style_weight) + ".model"
-        save_model_path = os.path.join(args.save_model_dir, save_model_filename)
-        torch.save(transformer.state_dict(), save_model_path)
+    # save model
+    transformer.eval()
+    transformer.cpu()
+    save_model_filename = "epoch_" + str(args.epochs) + "_" + str(time.ctime()).replace(' ', '_') + "_" + str(
+        args.content_weight) + "_" + str(args.style_weight) + ".model"
+    save_model_path = os.path.join(args.save_model_dir, save_model_filename)
+    torch.save(transformer.state_dict(), save_model_path)
 
-        print("\nDone, trained model saved at", save_model_path)
+    print("\nDone, trained model saved at", save_model_path)
 
 
 def check_paths(args):
